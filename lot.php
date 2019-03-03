1fbd1f7a131424662ce888da0b61625cd5ed05b8
nastya-randall <36882582+nastya-randall@users.noreply.github.com>
2019-03-03 15:31:55 +0100

    Revert "подвинула обработку формы наверх"
    This reverts commit cafde56a29aedff22fcec03b8ac42cc3eb957db4.

--- lot.php
+++ lot.php
@@ -7,8 +7,31 @@ $is_content = false;
 if (isset($_GET['id'])) {
   $lot_id = intval($_GET['id']);
 
+  $sql_one_lot = "SELECT l.id, l.name, l.description, l.start_price, l.image_path, COALESCE(MAX(b.bid), l.start_price) AS price, c.name AS cat_name, l.bid_incr, l.user_id
+  FROM lots l 
+  LEFT JOIN categories c ON l.category_id = c.id
+  LEFT JOIN bids b ON b.lot_id = l.id
+  WHERE l.dt_end > NOW()
+  AND l.id = '$lot_id'
+  GROUP BY l.id;";
+  $lot_result = mysqli_query($con, $sql_one_lot);
+  $lot = mysqli_fetch_assoc($lot_result);
+  $min_bid = get_min_bid($lot['price'], $lot['bid_incr']);
 
-  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+  if ($lot !== null) {
+    // находит ставки к лоту
+    $sql_get_bids = "SELECT u.name, b.bid, b.dt_add, b.user_id AS bid_author, l.user_id AS lot_author
+    FROM bids b
+    LEFT JOIN lots l ON b.lot_id = l.id
+    LEFT JOIN users u ON b.user_id = u.id
+    WHERE l.id = '$lot_id'
+    AND b.user_id != l.user_id
+    ORDER BY b.dt_add DESC;";
+    $bids_result = mysqli_query($con, $sql_get_bids);
+    $bids = mysqli_fetch_all($bids_result, MYSQLI_ASSOC);
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
       $form = $_POST;
       $errors = [];
 
@@ -40,37 +63,17 @@ if (isset($_GET['id'])) {
       ]);
 
       $res = mysqli_stmt_execute($stmt);
+      if ($res) {
+        $bids_result = mysqli_query($con, $sql_get_bids);
+        $bids = mysqli_fetch_all($bids_result, MYSQLI_ASSOC);
+        $min_bid = get_min_bid($lot['price'], $lot['bid_incr']);
+      }
 
       } else {
         $errors['cost'] = "Автор лота не может добавлять к нему ставки";
       }
     }
     }
-  $sql_one_lot = "SELECT l.id, l.name, l.description, l.start_price, l.image_path, COALESCE(MAX(b.bid), l.start_price) AS price, c.name AS cat_name, l.bid_incr, l.user_id
-  FROM lots l 
-  LEFT JOIN categories c ON l.category_id = c.id
-  LEFT JOIN bids b ON b.lot_id = l.id
-  WHERE l.dt_end > NOW()
-  AND l.id = '$lot_id'
-  GROUP BY l.id;";
-  $lot_result = mysqli_query($con, $sql_one_lot);
-  $lot = mysqli_fetch_assoc($lot_result);
-  $min_bid = get_min_bid($lot['price'], $lot['bid_incr']);
-
-
-  if ($lot !== null) {
-    // находит ставки к лоту
-    $sql_get_bids = "SELECT u.name, b.bid, b.dt_add, b.user_id AS bid_author, l.user_id AS lot_author
-    FROM bids b
-    LEFT JOIN lots l ON b.lot_id = l.id
-    LEFT JOIN users u ON b.user_id = u.id
-    WHERE l.id = '$lot_id'
-    AND b.user_id != l.user_id
-    ORDER BY b.dt_add DESC;";
-    $bids_result = mysqli_query($con, $sql_get_bids);
-    $bids = mysqli_fetch_all($bids_result, MYSQLI_ASSOC);
-
-    
 
     $content = include_template('lot-temp.php', [
       'categories' => $categories,
@@ -87,17 +90,17 @@ if (isset($_GET['id'])) {
   }
 }
 
-if (!$is_content) {
-    $title = '404 Страница не найдена';
-    $message = 'Данной страницы не существует на сайте.';
-    $content = include_template('error.php', [
-            'categories' => $categories,
-            'user_name' => $user_name,
-            'is_auth' => $is_auth,
-            'message' => $message,
-            'title' => $title
-    ]);
-}
+//if (!$is_content) {
+//    $title = '404 Страница не найдена';
+//    $message = 'Данной страницы не существует на сайте.';
+//    $content = include_template('error.php', [
+//            'categories' => $categories,
+//            'user_name' => $user_name,
+//            'is_auth' => $is_auth,
+//            'message' => $message,
+//            'title' => $title
+//    ]);
+//}
 
 $layout = include_template('layout.php', [
     'content' => $content,
@@ -107,5 +110,7 @@ $layout = include_template('layout.php', [
     'is_auth' => $is_auth
 ]);
 
+var_dump($lot);
+var_dump($errors);
 print($layout);
 ?>
